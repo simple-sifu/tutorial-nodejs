9038a049d2f675d0e3533137ee29eafbc857fd5d
simple-sifu <kattyntommy@gmail.com>
2023-04-16 06:33:06 -0400

    refactor

--- server/README.md
+++ server/README.md
@@ -1,5 +1,12 @@
 # Formating your code
+
 - npm i -D --EXACT prettier
 - npx prettier --write server.js
 - install extension Prettier - Code Formatter
+- make sure all linter are the same version or same linter. if team is using ESLint w/Prettier we
+  can install eslint-config-prettier
+- use .prettierignore for certain types of files
 
+# Versioning
+
+- define API with version 1 so it can be easily changed w/o affecting previous api.
--- server/server.js
+++ server/server.js
@@ -6,6 +6,32 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+app.get("/api/v1/products", (req, res) => {
+  res.status(200).json({ success: true, data: { name: "Brad" } });
+});
+
+app.get("/api/v1/products/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, message: `Show product ${req.params.id}` });
+});
+
+app.post("/api/v1/products", (req, res) => {
+  res.status(200).json({ success: true, message: "Create new product" });
+});
+
+app.put("/api/v1/products/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, message: `Update product ${req.params.id}` });
+});
+
+app.delete("/api/v1/products/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, message: `Delete product ${req.params.id}` });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
