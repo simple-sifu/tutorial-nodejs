9062a2b1363a1eb9539a49b0d8090350d7f68f62
simple-sifu <kattyntommy@gmail.com>
2023-04-16 03:57:39 -0400

    refactor

--- README.md
+++ README.md
@@ -3,5 +3,32 @@
 # Header
 - content
 - cookies
+- auth 
+
+# Setup
+- npm init -y
+- npm install --D nodemon
+
+# Postman is a headless browser we can use to test
+
+
+# HTTP Codes
+
+## 2.xx Success
+- 200 - Success
+- 201 - Created, new insert in db.
+- 204 - No content, delete request and return nothing.
+
+## 3.xx Redirection - something is moved
+- 304 - not modified - Get request but nothing is changed on server
+
+## 4.xx Client Error - clients fault
+- 400 - Bad Request - submitted a form with bad email. Did not give what was needed to be successful
+- 401 - Unauthorized - no access, trying to access something, that requires a login authentication
+- 404 - Not Found - not there
+
+## 5.xx Server Error
+- 500 - Internal Server Error
+
 
 
--- nodeServer.js
+++ nodeServer.js
@@ -1,9 +1,38 @@
 const http = require('http');
 
+const todos = [
+    { 
+        id: 1, text: 'Todo One'
+    },
+    { 
+        id: 2, text: 'Todo Two'
+    },
+    { 
+        id: 3, text: 'Todo Three'
+    },
+]
+
 const server = http.createServer((req, res) => {
+    // returns key value pairs - req headers, url, methods
     const { headers, url, method } = req;
     console.log(`method=${method},  url=${url}, header= ${JSON.stringify(headers)}`);
-    res.end();
+    // if we open in browser for text/plain then it wont render.
+    // change to text/html to render
+    // res.setHeader('Content-Type', 'text/html')
+    res.setHeader('Content-Type', 'application/json')
+    res.setHeader('X-Powered-By', 'Node.js')
+    // res.write("<h1>Hello<h1>")
+    // res.write("<h1>Hello Again<h1>")
+    // res.write("<h1>Hello Again Again<h1>")
+    // allows request to end and return "200" response...
+
+    // We dont need to use res.write and could put it in response 
+    // instead but we have to make sure we stringify the data first
+    // before we send it back.
+    res.end(JSON.stringify({
+        success: true,
+        data: todos
+    }));
 })
 
 const PORT = 8000;
