f4295d3f8be8cdf3e387e47b6873a022cc296007
simple-sifu <kattyntommy@gmail.com>
2023-04-19 17:32:59 -0400

    fix eslint issue.

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,15 @@ module.exports = {
   ],
   rules: {
   },
+  'import/no-extraneous-dependencies': [
+    'error',
+    {
+      devDependencies: [
+        '**/*.test.ts',
+        '**/*.test.tsx',
+      ],
+    },
+  ],
   settings: {
     'import/resolver': {
       node: {
--- client/index.jsx
+++ client/index.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+
+import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+import { configureStore } from '@reduxjs/toolkit';
 import App from './App';
 
-const container = document.getElementById('root');
+const saga = createSagaMiddleware();
+const store = configureStore(
+  {
+    reducer: {},
+    middleware: [saga],
+  },
+);
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <App tab="home" />
+  </Provider>,
 
-const root = createRoot(container);
-root.render(<App tab="home" />);
+);
